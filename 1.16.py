13e14e04b6290f424ab169dba3aa2ead736209c7
wuyibo <wuyibo@cmcm.com>
2018-09-11 18:12:56 +0800

    remove useless code

--- 1.16.py
+++ 1.16.py
@@ -8,34 +8,7 @@ def fast_expt(base, n):
         return base * fast_expt(base, n - 1)
 
 
-def fast_expt2(base, n):
-    return expt_iter(base, n, n, 1) 
-    
-
-def expt_iter(base, n, old, produce):
-    if n == 0:
-        return produce
-    elif n % 2 == 0:
-        if produce == 1:
-            return expt_iter(base, n - 1, old, base)
-        else:
-            return expt_iter(base, n // 2, old, produce * produce)
-    else:
-        if n == 1 and old % 2 == 0:
-            return expt_iter(base, n - 1, produce * produce)
-        else:
-            return expt_iter(base, n - 1, base * produce)
-
-
-def foo(base, n):
-
-
-
 base = 2
 for n in range(11):
     ret = fast_expt(base, n)
-    print(n, ret)
-
-for n in range(11):
-    ret = fast_expt2(base, n)
     print(n, ret)
